284f26d454ee8aa8bcd7d364d1d727502a9cccd4
Henrique <pessolatohenrique@gmail.com>
2020-02-25 19:42:34 -0300

    Layout para pagina de noticias

--- news/views.py
+++ news/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from subject.models import Subject
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html')
+    user = User.objects.get(id=2)
+
+    categories = Subject.objects.filter(users=user).order_by('name')
+    return render(request, 'index.html', {
+        'categories': categories
+    })
