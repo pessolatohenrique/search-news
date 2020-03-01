ae2e7fc2c53c4c51fee9307362e605f88c43f464
Henrique <pessolatohenrique@gmail.com>
2020-03-01 12:02:16 -0300

    Capturando noticias de acordo com usuario logado

--- news/views.py
+++ news/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 
 
 def index(request):
-    user = User.objects.get(id=2)
+    user = User.objects.get(id=request.user.id)
 
     categories = Subject.objects.filter(users=user).order_by('name')
     categories_timeline = categories[0:3]
@@ -25,7 +25,7 @@ def index(request):
 def search(request):
     category_id = request.GET.get('id')
     site_id = request.GET.get('site_id')
-    user = User.objects.get(id=2)
+    user = User.objects.get(id=request.user.id)
 
     categories = Subject.objects.filter(users=user).order_by('name')
     sites = Site.objects.order_by('name')
@@ -45,7 +45,7 @@ def search(request):
 
 
 def view(request, id):
-    user = User.objects.get(id=2)
+    user = User.objects.get(id=request.user.id)
 
     categories = Subject.objects.filter(users=user).order_by('name')
     sites = Site.objects.order_by('name')
