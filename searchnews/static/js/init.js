8898344a756c50a94017e1c214d61d4f4fa1cf34
Henrique <pessolatohenrique@gmail.com>
2020-03-08 14:25:04 -0300

    Componente de pesquisa dinamica

--- searchnews/static/js/init.js
+++ searchnews/static/js/init.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
   $(".dropdown-trigger").dropdown();
   $(".sidenav").sidenav();
+  $(".btn-search").on("click", function(event) {
+    event.preventDefault();
+    $(".card-search").toggle();
+  });
 });
